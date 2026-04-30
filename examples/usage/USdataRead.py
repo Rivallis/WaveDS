c4baecbfaaba3a74edc15168bc679857d95237ca
Jiaxing Ye <rivallis@gmail.com>
2026-04-30 16:11:46 +0900

    Update USdataRead.py

--- examples/usage/USdataRead.py
+++ examples/usage/USdataRead.py
@@ -5,8 +5,6 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-
-@author: Jiaxing
 "This piece of code is the hub file that converts MATLAB data files to Python format"""
 
 import numpy as np
@@ -68,64 +66,3 @@ USIplateNGidx = torch.tensor(mat['USIplateIdxNG'])
 USIplateOKidx = torch.tensor(mat['USIplateIdxOK'])
 USIplateNGlabel = torch.tensor(mat['USIplateLabelNG'])
 USIplateOKlabel = torch.tensor(mat['USIplateLabelOK'])
-
-
-
-# =============================================================================
-
-# USimg500Hz = f.get('USdataAug') 
-# USimg500Hz = np.array(USimg500Hz)
-# #USimg500Hz = np.transpose(USimg500Hz, (3,2,1,0))
-# USimg500Hz = torch.tensor(USimg500Hz/255, dtype = torch.float32) 
-# USimg500HzAdd = USimg500Hz[0:2204,:,:,:]
-# USimg500Hz = USimg500Hz[2204::,:,:,:]
-
-
-# label500Hz = f.get('label500Hz')
-# label500Hz = np.array(label500Hz)
-# label500Hz = torch.tensor(label500Hz, dtype = torch.int16) 
-# label500HzAdd = label500Hz[0:2204]
-# label500Hz = label500Hz[2204::]
-
-# plateIdx = f.get('plateIdxExt') 
-# plateIdx = np.array(plateIdx)
-# plateIdx = torch.tensor(plateIdx, dtype = torch.int16) 
-# plateIdxAdd = plateIdx[0:2204]
-# plateIdx = plateIdx[2204::]
-
-# NUM_IMG = np.int(len(plateIdx)/6)
-
-# USimg500HzOrig = USimg500Hz[0 : NUM_IMG]
-# label500HzOrig = label500Hz[0 : NUM_IMG]
-# plateIdxOrig = plateIdx[0 : NUM_IMG]
-
-# #
-# # =============================================================================
-
-# #p = h5py.File('USimgOK224.mat','r') 
-# USimgOK = f.get('USimgGrayOK')
-# USimgOK = np.array(USimgOK)
-# #USimgOKgray = np.int16(rgb2gray(USimgOK))
-# USimgOKgray = torch.tensor(USimgOK/255, dtype = torch.float32) 
-
-# #from imblearn.over_sampling import KMeansSMOTE
-# #X_resampled, y_resampled =  KMeansSMOTE().fit_resample(USimgOK, np.ones())
-
-# # =============================================================================
-# index = np.arange(len(label500Hz)-NUM_IMG)  ####
-# np.random.shuffle(index)
-# index = torch.tensor(NUM_IMG+index, dtype = torch.int64)
-# index = index[0:np.int(len(label500Hz)/4)]
-
-# USimg500HzAug = USimg500Hz[index]
-# plateIdxAug = plateIdx[index]
-# label500HzAug = label500Hz[index]
-
-# USimg500Hz = torch.cat((USimg500HzOrig, USimg500HzAug), dim = 0)
-# USimg500Hz = torch.cat((USimg500Hz, USimg500HzAdd), dim = 0)
-# label500Hz = torch.cat((label500HzOrig, label500HzAug), dim = 0)
-# label500Hz = torch.cat((label500Hz, label500HzAdd), dim = 0)
-# plateIdx = torch.cat((plateIdxOrig, plateIdxAug), dim = 0)
-# plateIdx = torch.cat((plateIdx, plateIdxAdd), dim = 0)
-# #pickle.dump([USimg500Hz, label500Hz, plateIdx], open("US_DB_Aug.p", "wb" ))
-# #pickle.dump([USimg500HzOrig, label500HzOrig, plateIdxOrig], open("US_DB_orig.p", "wb" ), protocol=4)
